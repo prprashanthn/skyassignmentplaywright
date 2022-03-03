087b7529b04eb164bf2d6c17ad9387133bb52e88
Prashanth Nomula <prprashanthn@gmail.com>
2022-03-03 14:01:39 +0530

    Updated READ me file

--- PageObjects/signin.page.js
+++ PageObjects/signin.page.js
@@ -1,30 +1,30 @@
 const { expect } = require("@playwright/test");
 
-
-
 class SignInPage{
+
     async enterUserName(userName){
-        await page.waitForSelector('iframe[title=\'iFrame containing Sky Sign-In application\']');
-        const elementHandle = await page.$('iframe[title=\'iFrame containing Sky Sign-In application\']');   
-        const frame = await elementHandle.contentFrame();
+        const frame = await getIframe();
         await frame.locator('#username').fill(userName);
     }
 
     async clickContButton(){
         await page.screenshot({ path: 'screenshot.png', fullPage: false });
-        await page.waitForSelector('iframe[title=\'iFrame containing Sky Sign-In application\']');
-        const elementHandle = await page.$('iframe[title=\'iFrame containing Sky Sign-In application\']');   
-        const frame = await elementHandle.contentFrame();
+        const frame = await getIframe();
         await frame.locator('[data-testid="AUTHN__SUBMIT_BTN"]').click();
     }
 
     async verifyCreatePasstext(expPasswordText){
         await page.screenshot({ path: 'screenshot.png', fullPage: true });
-        await page.waitForSelector('iframe[title=\'iFrame containing Sky Sign-In application\']');
-        const elementHandle = await page.$('iframe[title=\'iFrame containing Sky Sign-In application\']');   
-        const frame = await elementHandle.contentFrame();
+        const frame = await getIframe();
         expect(await frame.locator('//h1[contains(text(),"Create your My Sky")]')).toContainText(expPasswordText);
 
     }
 }
 module.exports = { SignInPage };
+
+async function getIframe() {
+    await page.waitForSelector('iframe[title=\'iFrame containing Sky Sign-In application\']');
+    const elementHandle = await page.$('iframe[title=\'iFrame containing Sky Sign-In application\']');
+    const frame = await elementHandle.contentFrame();
+    return frame;
+}
